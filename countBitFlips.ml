627ac5e076196c82b75ab600f0f5e15737abb052
Nico? <84991759+NiccoDorn@users.noreply.github.com>
2025-05-23 18:07:15 +0200

    fixed 0, b and a, 0 cases

--- countBitFlips.ml
+++ countBitFlips.ml
@@ -9,14 +9,15 @@ let countBitFlips a b =
   in cnt a' 0;;
   
 let numBitFlips = countBitFlips bitseq_a bitseq_b;;
+Printf.printf "%d\n" numBitFlips;;
 
 (* Checking both bit sequences with the land 1 operation to get LSB *)
 let checkDiffBit a b = if (a land 1) = (b land 1) then 0 else 1;;
 let countDiffBits a b =
   let rec cnt' a b c = match a, b with
     | 0, 0 -> c
-    | 0, b -> c
-    | a, 0 -> c
+    | 0, b -> cnt' a (b lsr 1) (c + checkDiffBit a b)
+    | a, 0 -> cnt' (a lsr 1) b (c + checkDiffBit a b)
     | a, b -> cnt' (a lsr 1) (b lsr 1) (c + checkDiffBit a b)
   in cnt' a b 0;;
 
@@ -25,3 +26,8 @@ let numDiffBits = countDiffBits 149857 203495;;
 (* Testing *)
 let test_true = (countBitFlips 149857 203495) = (countDiffBits 149857 203495);;
 let test_false = (countBitFlips 149857 203495) = (countDiffBits 149857 203494);;
+
+let al = 0b1100000000000000000000001;; 
+let bl = 0b0000000000000000000000001;;
+
+let test = (countBitFlips al bl) = 2 && (countDiffBits al bl) = 2;;
